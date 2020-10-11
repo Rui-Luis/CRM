92bc125aed514161de79ce69bed8d465edfaa201
Rui Luis <ruidcluis@gmail.com>
2020-10-11 20:08:11 +0100

    Added basic CRUD functionality to server

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 // Entry point for the server
 import express from 'express';
+import routes from './src/routes/crmRoutes';
 
 const app = express();
 const PORT = 4000;
+
+routes(app);
 
 app.get('/', (req, res) =>
     res.send(`Node and express server running on port ${PORT}`)
@@ -11,4 +14,3 @@ app.get('/', (req, res) =>
 app.listen(PORT, () =>
     console.log(`The server is running on port ${PORT}`)
 );
-
